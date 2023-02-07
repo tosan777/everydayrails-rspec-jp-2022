70fcdf18b8000c0a589d7a72e0c5cd47efc9f00e
tosan777 <ksk.pleiades@gmail.com>
2023-02-07 14:05:19 +0900

    systemテストを追加しました。

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -25,4 +25,45 @@ RSpec.describe "Tasks", type: :system do
     expect(page).to_not have_css "label#task_#{task.id}.completed"
     expect(task.reload).to_not be_completed
   end
+
+  # ユーザーがタスクを作成する
+  scenario "user create task" do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project, owner: user)
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect {
+      click_link "Project 1"
+      click_link "Add Task"
+      fill_in "task[name]", with: "meeting"
+      click_button "Create Task"
+      expect(page).to have_content "Task was successfully created."
+      expect(page).to have_content "meeting"
+    }.to change(project.tasks, :count).by(1)
+  end
+
+  # ユーザーがタスクを削除する
+  scenario "user destroy task", js: true do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project, owner: user)
+    task = project.tasks.create(name: 'タスク')
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect {
+      click_link "Project 2"
+      click_link "Delete"
+      expect(page.accept_confirm).to eq "Are you sure?"
+      expect(page).to have_content "Task was successfully destroyed."
+    }.to change(project.tasks, :count).by(-1)
+  end
 end
